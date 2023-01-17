2708849f96f94f4900ec9a82c92f45889802d8e7
yoony <code.sugartoast@gmail.com>
2023-01-18 02:27:39 +0900

    daemon에 이벤트 매핑 작업중

--- go.mod
+++ go.mod
@@ -28,6 +28,7 @@ require (
 	github.com/goccy/go-json v0.9.11 // indirect
 	github.com/golang/snappy v0.0.4 // indirect
 	github.com/google/uuid v1.2.0 // indirect
+	github.com/holiman/uint256 v1.2.1 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
 	github.com/klauspost/compress v1.13.6 // indirect
 	github.com/leodido/go-urn v1.2.1 // indirect
--- model/daemon.go
+++ model/daemon.go
@@ -3,15 +3,21 @@ package model
 import (
 	"context"
 	"fmt"
+	"io/ioutil"
 	log "lecture/WBA-BC-Project-04/logger"
 	"math/big"
+	"path/filepath"
+	"strings"
 	"time"
 
 	"github.com/ethereum/go-ethereum"
+	"github.com/ethereum/go-ethereum/accounts/abi"
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/core/types"
+	"github.com/ethereum/go-ethereum/crypto"
 	"github.com/ethereum/go-ethereum/ethclient"
 	"github.com/ethereum/go-ethereum/event"
+	"github.com/holiman/uint256"
 )
 
 type Block struct {
@@ -36,6 +42,12 @@ type Transaction struct {
 	BlockNumber uint64 `bson:"blockNumber"`
 }
 
+type createMatchEvent struct {
+	P1      string `bson:"p1"`
+	P2      string `bson:"p2"`
+	MatchId string `bson:"matchId"`
+}
+
 func (p *Model) RunDaemon(quit chan bool) {
 	fmt.Println("run daemon")
 	// ethclint 초기화
@@ -50,12 +62,22 @@ func (p *Model) RunDaemon(quit chan bool) {
 	}
 	// subscribe
 	logs := make(chan types.Log)
+	abiPath, _ := filepath.Abs("./build/Game.abi")
+	file, err := ioutil.ReadFile(abiPath)
+	if err != nil {
+		log.Error(err.Error())
+	}
 
 	sub := event.Resubscribe(10*time.Second, func(ctx context.Context) (event.Subscription, error) {
 		return client.SubscribeFilterLogs(context.Background(), query, logs)
 	})
+	gameAbi, err := abi.JSON(strings.NewReader(string(file)))
+	fmt.Println("subscribe: ", gameAbi)
 
-	fmt.Println("subscribe")
+	logCreateMatchSig := []byte("CreateMatchEvent(address,address,uint256)")
+	logMatchEndSig := []byte("MatchEndEvent(uint256,int256)")
+	logCreateMatchSigHash := crypto.Keccak256Hash(logCreateMatchSig)
+	logMatchEndSigHash := crypto.Keccak256Hash(logMatchEndSig)
 
 	for {
 		select {
@@ -67,59 +89,91 @@ func (p *Model) RunDaemon(quit chan bool) {
 			log.Error(err.Error())
 			return
 		case scribelog := <-logs:
-			block, err := client.BlockByNumber(context.Background(), new(big.Int).SetUint64(scribelog.BlockNumber))
+			switch scribelog.Topics[0].Hex() {
+			case logCreateMatchSigHash.Hex():
+				var createMatchEvent = createMatchEvent{}
 
-			// 블록 구조체 생성
-			b := Block{
-				BlockHash:    block.Hash().Hex(),
-				BlockNumber:  block.Number().Uint64(),
-				GasLimit:     block.GasLimit(),
-				GasUsed:      block.GasUsed(),
-				Time:         block.Time(),
-				Nonce:        block.Nonce(),
-				Transactions: make([]Transaction, 0),
-			}
-
-			// 트랜잭션 추출
-			txs := block.Transactions()
-			if len(txs) > 0 {
-				for _, tx := range txs {
-					msg, err := tx.AsMessage(types.LatestSignerForChainID(tx.ChainId()), block.BaseFee())
-					if err != nil {
-						log.Error(err.Error())
-					}
-
-					// 트랜잭션 구조체 생성
-					t := Transaction{
-						TxHash:      tx.Hash().Hex(),
-						To:          "", // 디폴트 값 처리
-						From:        msg.From().Hex(),
-						Nonce:       tx.Nonce(),
-						GasPrice:    tx.GasPrice().Uint64(),
-						GasLimit:    tx.Gas(),
-						Amount:      tx.Value().Uint64(),
-						BlockHash:   block.Hash().Hex(),
-						BlockNumber: block.Number().Uint64(),
-					}
-
-					if tx.To() != nil {
-						t.To = tx.To().Hex()
-					}
-
-					b.Transactions = append(b.Transactions, t)
+				matchId, err := uint256.FromHex(scribelog.Topics[3].Hex())
+				if err != nil {
+					log.Error(err.Error())
 				}
-			}
+				createMatchEvent.P1 = scribelog.Topics[1].String()
+				createMatchEvent.P2 = scribelog.Topics[2].String()
+				createMatchEvent.MatchId = scribelog.Topics[3].String()
+				// DB insert
+				err = p.SaveCreateMatchEvent(&createMatchEvent)
+				if err != nil {
+					log.Error(err.Error())
+				}
+				fmt.Printf("SaveCreateMatchEvent: %v ", matchId)
+			case logMatchEndSigHash.Hex():
+			default:
 
-			// DB insert
-			err = p.SaveBlock(&b)
-			if err != nil {
-				log.Error(err.Error())
+				block, err := client.BlockByNumber(context.Background(), new(big.Int).SetUint64(scribelog.BlockNumber))
+
+				// 블록 구조체 생성
+				b := Block{
+					BlockHash:    block.Hash().Hex(),
+					BlockNumber:  block.Number().Uint64(),
+					GasLimit:     block.GasLimit(),
+					GasUsed:      block.GasUsed(),
+					Time:         block.Time(),
+					Nonce:        block.Nonce(),
+					Transactions: make([]Transaction, 0),
+				}
+
+				// 트랜잭션 추출
+				txs := block.Transactions()
+				if len(txs) > 0 {
+					for _, tx := range txs {
+						msg, err := tx.AsMessage(types.LatestSignerForChainID(tx.ChainId()), block.BaseFee())
+						if err != nil {
+							log.Error(err.Error())
+						}
+
+						// 트랜잭션 구조체 생성
+						t := Transaction{
+							TxHash:      tx.Hash().Hex(),
+							To:          "", // 디폴트 값 처리
+							From:        msg.From().Hex(),
+							Nonce:       tx.Nonce(),
+							GasPrice:    tx.GasPrice().Uint64(),
+							GasLimit:    tx.Gas(),
+							Amount:      tx.Value().Uint64(),
+							BlockHash:   block.Hash().Hex(),
+							BlockNumber: block.Number().Uint64(),
+						}
+
+						if tx.To() != nil {
+							t.To = tx.To().Hex()
+						}
+
+						b.Transactions = append(b.Transactions, t)
+					}
+				}
+
+				// DB insert
+				err = p.SaveBlock(&b)
+				if err != nil {
+					log.Error(err.Error())
+				}
 			}
 		}
 	}
 }
 func (p *Model) SaveBlock(block *Block) error {
 	_, err := p.gameCol.InsertOne(context.TODO(), block)
+	if err != nil {
+		log.Error(err.Error())
+		return err
+	}
+	fmt.Println("Insert succeed")
+
+	return nil
+}
+
+func (p *Model) SaveCreateMatchEvent(event *createMatchEvent) error {
+	_, err := p.gameCol.InsertOne(context.TODO(), event)
 	if err != nil {
 		log.Error(err.Error())
 		return err
